ac542266006c76a0408cbf708731f974230782a4
TJ <thammahetti.m.t@iisgalvanimi.edu.it>
2025-01-30 08:49:00 +0000

    Da finire parte in autonomia

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from spotipy.oauth2 import SpotifyOAuth
 #le tue credenziali le trovi nella dashboard di prima
 SPOTIFY_CLIENT_ID = "51732c4254124416b138a4114f3399f9"
 SPOTIFY_CLIENT_SECRET = "71185d62c08749e6ab66a9e42507b90a"
-SPOTIFY_REDIRECT_URI = "https://supreme-engine-975qg7r6rjg2x9g7-5000.app.github.dev/" 
+SPOTIFY_REDIRECT_URI = "https://supreme-engine-975qg7r6rjg2x9g7-5000.app.github.dev/callback" 
 
 app = Flask(__name__)
 app.secret_key = 'chiave_per_session'
@@ -14,7 +14,8 @@ sp_oauth = SpotifyOAuth(
 client_id=SPOTIFY_CLIENT_ID,
 client_secret=SPOTIFY_CLIENT_SECRET,
 redirect_uri=SPOTIFY_REDIRECT_URI,
-scope="user-read-private" #p
+scope="user-read-private",
+show_dialog=True #forziamo la richiesta di inserire new credenziali
 )
 
 
@@ -39,7 +40,22 @@ def home():
     sp = spotipy.Spotify(auth=token_info['access_token'])
     user_info = sp.current_user()
     print(user_info) 
-    return render_template('home.html', user_info=user_info) 
+    playlists = sp.current_user_playlists() #sempre tramite il token sp preso prima
+    playlists_info = playlists['items'] #prendiamo solo la lista delle playlist
+    return render_template('home.html', user_info=user_info, playlists=playlists_info) 
+
+@app.route('/playlist/<playlist_id>')
+def playlist_details():
+    brani = sp.playlist_items(playlists['id'])
+    brani_specifici = brani['items']
+    return render_template('brani.html')
+
+
+
+@app.route('/logout')
+def logout():
+    session.clear() #cancelliamo l'access token salvato in session
+    return redirect(url_for('login'))
 
 if __name__ == '__main__': 
     
